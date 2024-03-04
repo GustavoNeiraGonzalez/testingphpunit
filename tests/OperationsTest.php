dd86dba37c236f1478f1b315e64a897ad3c75ab6
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2024-03-03 23:57:18 -0300

    agregado mensaje en caso de no pasar la prueba de suma de valores (en este caso con que no corresponda al valor esperado ya da el mensaje, no si es nulo o no)

--- tests/OperationsTest.php
+++ tests/OperationsTest.php
@@ -13,8 +13,23 @@ class OperationsTest extends TestCase{
     //aqui la funcion de phpunit assertEquals busca que el valor sea 7
     //despues de la coma se busca la funcion sum que creamos anteriormente
     //la cual sumara valores
+    //agregado mensaje en caso de error: "la suma de dos..." este mensaje se 
+    //lanza si el error del test no pasa la prueba
     public function testSumWithTwoValues(){
-        $this->assertEquals(7, $this->op->sum(24,5));
+        $this->assertEquals(7, $this->op->sum(2,NUll), 'La suma de dos valores debe devolver valores numericos');
+    }
+    //Verificar suma con valores nulos (no da error por valor nulo Y si esperas
+    //un valor numerico dara error de numero no esperado, lo cual es erroneo,
+    //y si esperas un valor nulo, dara un ok si das 2 valores nulos)
+    //por lo que se debe arreglar dentro de la funcion sum
+
+    public function testSumWithNullValues(){
+        $this->assertEquals(NULL, $this->op->sum(NULL, NULL));
+    }
+    public function testSumWithNullValuesException(){
+        $this->expectException(InvalidArgumentException::class);
+        $this->op->sum(NULL,NULL);
+
     }
 }
 ?>
