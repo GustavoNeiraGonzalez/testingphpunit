7eddd9bd53fb00cb84f6d6aaee9bfe25fc1c8c69
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2024-03-10 16:51:17 -0300

    mejorada explicacion dentro del codigo para la validacion de excepciones

--- tests/OperationsTest.php
+++ tests/OperationsTest.php
@@ -18,10 +18,9 @@ class OperationsTest extends TestCase{
     public function testSumWithTwoValues(){
         $this->assertEquals(7, $this->op->sum(5,2), 'La suma de dos valores debe devolver valores numericos');
     }
-    //Verificar suma con valores nulos (no da error por valor nulo Y si esperas
-    //un valor numerico dara error de numero no esperado, lo cual es erroneo,
-    //y si esperas un valor nulo, dara un ok si das 2 valores nulos)
-    //por lo que se debe arreglar dentro de la funcion sum
+    //Verificar suma con valores nulos (solo a modo de prueba, realmente no 
+    //se necesita que los valores sean nulos en una suma)
+    //por lo que se debe arreglar dentro de la funcion suma para mandar una excepcion
 
     /*
     public function testSumWithNullValues(){
@@ -30,12 +29,14 @@ class OperationsTest extends TestCase{
     */
     // esta funcion sirve para validar un error en especifico en este caso
     // invalidargument, que se refiere a que no es un valor numerico el cual
-    // se espera en esta suma ademas este error se debe lanzar manualmente en un if
-    // es decir validar que sean valores numericos en la operacion de suma
+    // se espera en esta suma recordar que esta excecion se usa en los ifs dentro
+    // de la funcion suma, por ejemplo si no validas que sean null o string no 
+    // dara la excepción
     public function testSumWithNullValuesException(){
         $this->expectException(InvalidArgumentException::class);
         $this->op->sum(NULL,NULL);
     }
+    //aqui modifique la funcion suma para que verifique los strings tambien 
     public function testSumWithNotNumericValuesException(){
         $this->expectException(InvalidArgumentException::class);
         $this->op->sum('a','hello');
